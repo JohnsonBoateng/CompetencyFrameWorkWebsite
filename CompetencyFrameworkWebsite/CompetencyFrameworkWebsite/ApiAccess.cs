88954d9c43991b08362981006402b1bec788ee04
JohnsonBoateng <johnson.boateng@bglgroup.co.uk>
2017-12-15 11:17:41 +0000

    Now calling Api to get Technology results

---
+++ CompetencyFrameworkWebsite/CompetencyFrameworkWebsite/ApiAccess.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Remoting.Messaging;
+using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace CompetencyFrameworkWebsite
+{
+    public class ApiAccess
+    {
+        private HttpClient _httpClient;
+
+        public ApiAccess()
+        {
+            _httpClient = new HttpClient {BaseAddress = new Uri(ConfigurationManager.AppSettings["ApiBaseAddress"]) };
+        }
+
+        public List<string> GetAllTechnologies()
+        {       
+            return CallApi("api/technology");
+        }
+
+        private List<string> CallApi(string address)
+        {
+            var response = _httpClient.GetAsync(address).Result;
+            var result = response.Content.ReadAsStringAsync().Result;
+            return JsonConvert.DeserializeObject<List<string>>(result);
+        }
+    }
+}
---
+++ CompetencyFrameworkWebsite/CompetencyFrameworkWebsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CompetencyFrameworkWebsite.MvcApplication" Language="C#" %>
