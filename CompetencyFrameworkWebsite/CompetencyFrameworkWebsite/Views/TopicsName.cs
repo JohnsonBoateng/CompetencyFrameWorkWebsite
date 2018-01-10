e76a274945ee3d5fabde29e36b46cc1f97bafafe
mala <mala.aboidje@bglgroup.co.uk>
2018-01-10 16:25:07 +0000

    created table displaying results based on technology name and jobtitle

---
+++ CompetencyFrameworkWebsite/CompetencyFrameworkWebsite/Views/TopicsName.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CompetencyFrameworkWebsite
+{
+    public class Topics
+    {
+        public string TopicName { get; set; }
+    }
+}
